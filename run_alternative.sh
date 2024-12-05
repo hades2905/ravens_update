5059397ad717f10c1610515b2a82d9a877458172
hades2905 <167974170+hades2905@users.noreply.github.com>
2024-12-06 00:27:41 +0100

    Update run_alternative.sh

--- run_alternative.sh
+++ run_alternative.sh
@@ -2,12 +2,12 @@ TASK=$1
 AGENT=$2
 ASSETS_ROOT=./ravens/environments/assets/
 
-python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1 --n_steps=20000
-python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=10 --n_steps=20000
-python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100 --n_steps=20000
-python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1000 --n_steps=20000
+python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1 
+python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=10 
+python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100 
+python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1000 
 
-python ravens/plot.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1
-python ravens/plot.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=10
-python ravens/plot.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100
-python ravens/plot.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1000
+python ravens/plot.py  --task=${TASK} --agent=${AGENT} --n_demos=1
+python ravens/plot.py  --task=${TASK} --agent=${AGENT} --n_demos=10
+python ravens/plot.py  --task=${TASK} --agent=${AGENT} --n_demos=100
+python ravens/plot.py  --task=${TASK} --agent=${AGENT} --n_demos=1000
