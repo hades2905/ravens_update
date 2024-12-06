d487c7ed43f03f1bf469da1120f6519aed73de76
hades2905 <167974170+hades2905@users.noreply.github.com>
2024-12-06 01:03:06 +0100

    Update run_alternative.sh

--- run_alternative.sh
+++ run_alternative.sh
@@ -2,6 +2,11 @@ TASK=$1
 AGENT=$2
 ASSETS_ROOT=./ravens/environments/assets/
 
+python ravens/train.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1 
+python ravens/train.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=10 
+python ravens/train.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100 
+python ravens/train.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1000 
+
 python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=1 
 python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=10 
 python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100 
