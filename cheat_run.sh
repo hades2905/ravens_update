015f72f81a01b7e823634ec5547b2257812caa77
hades2905 <s.ertlmaier@t-online.de>
2024-12-06 01:21:36 +0100

    cheat

---
+++ cheat_run.sh
@@ -0,0 +1,10 @@
+TASK=$1
+AGENT=$2
+ASSETS_ROOT=./ravens/environments/assets/
+
+
+python ravens/train.py --task=${TASK} --agent=${AGENT} --n_demos=100 --n_steps=20000
+
+python ravens/test.py  --assets_root=$ASSETS_ROOT --task=${TASK} --agent=${AGENT} --n_demos=100 --n_steps=20000
+
+python ravens/plot.py  --task=${TASK} --agent=${AGENT} --n_demos=100
